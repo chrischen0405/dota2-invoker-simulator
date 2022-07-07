7d611c28c813b05c983a82fedcd44d2744dd60ed
chrischen0405 <chrischen0405@gmail.com>
2022-07-07 23:59:58 +0800

    chore: work

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container" :class="isMobile ? 'mobile-container' : ''">
+      <div class="want-skill-box">
+        <img v-if="wantSkillCode"
+             :src="require(`@/assets/img/skill${wantSkillCode}.png`)"
+             :alt="wantSkillCode">
+      </div>
+      <div class="video-box">
+        <video
+            v-if="skillCode"
+            autoplay="autoplay"
+            preload="auto"
+            loop="loop"
+            playsinline
+            :src="require(`@/assets/video/skill${skillCode}.mp4`)"></video>
+      </div>
+      <div class="my-key-list">
+        <div v-for="(letter, index) in letterList"
+             class="skill-box"
+             :key="index">
+          <img :src="require(`@/assets/img/skill${letter}.png`)" :alt="letter">
+        </div>
+        <div class="skill-box">
+          <img v-if="skillCode"
+               :src="require(`@/assets/img/skill${skillCode}.png`)"
+               :alt="skillCode">
+        </div>
+      </div>
+      <div class="operate-box">
+        <div v-for="(letter, index) in btnList"
+             class="skill-btn"
+             @click="clickBtn(letter)"
+             :key="index">
+          <img :src="require(`@/assets/img/skill${letter}.png`)" :alt="letter">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {
+  allowKey,
+  keyToLetterMap,
+  skillMap
+} from '@/common/data'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      isMobile: true,
+      wantSkillCode: 'Y',
+      btnList: ['Q', 'W', 'E', 'R'],
+      letterList: [],
+      skillCode: ''
+    }
+  },
+  mounted() {
+    const info = navigator.userAgent
+    this.isMobile = /mobile/i.test(info)
+    this.watchKeyBoard()
+  },
+  methods: {
+    watchKeyBoard () {
+      document.onkeydown = e => {
+        const key = e.keyCode
+        if (allowKey.includes(key)) {
+          const letter = keyToLetterMap[key]
+          this.clickBtn(letter)
+        }
+      }
+    },
+    clickBtn (letter) {
+      if (letter === 'R') { // 切技能
+        if (this.letterList.length === 3) {
+          this.skillCode = skillMap[this.letterList.join('')]
+        }
+      } else {
+        this.pushLetter(letter)
+      }
+    },
+    pushLetter (letter) {
+      if (this.letterList.length === 3) {
+        this.letterList.splice(0, 1)
+      }
+      this.letterList.push(letter)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./common/styles/app.css";
 </style>
