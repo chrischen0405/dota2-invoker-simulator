cf0d9959f0e844c465fe94f3bed85a6604cb2683
chrischen0405 <chrischen0405@gmail.com>
2022-07-09 17:42:15 +0800

    feat: 新增自动进行游戏功能

--- src/App.vue
+++ src/App.vue
@@ -90,6 +90,7 @@ export default {
       this.endTime = new Date((new Date()).getTime() + 30000)
       this.isEndGame = false
       this.runTime()
+      // this.startAutoPlay(200)
     },
     runTime () {
       if (this.timer) return
@@ -149,7 +150,38 @@ export default {
         newSkillCode = allSkillList[getRandomNumber10()]
       }
       this.wantSkillCode = newSkillCode
-    }
+    },
+    startAutoPlay (time) { // 作弊自动玩
+      this.autoplay(time)
+      window.setInterval((time) => {
+        this.autoplay(time)
+      }, time)
+    },
+    autoplay (time) {
+      if (this.isEndGame) return
+      let flag = false
+      for (let key in skillMap) {
+        if (flag) return
+        if (skillMap[key] === this.wantSkillCode) {
+          flag = true
+          const keyList = key.split('')
+          // 每个按键之间增加间隔模仿真人按键
+          window.setTimeout(() => {
+            this.clickBtn(keyList[0])
+          }, Math.floor(time / 4))
+          window.setTimeout(() => {
+            this.clickBtn(keyList[1])
+          }, Math.floor(time / 2))
+          window.setTimeout(() => {
+            this.clickBtn(keyList[2])
+          }, Math.floor(time / 4 * 3))
+          window.setTimeout(() => {
+            this.clickBtn('R')
+          }, Math.floor(time))
+        }
+      }
+    },
+
   }
 }
 </script>
